b5910e94abb792662f69e417a8d883a056ea2709
Marcin Walas <walasiek@gmail.com>
2023-01-22 15:01:54 +0100

    preprocessor/text_cleaners added  + tests

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ai-polit
 AI for politics. My toys to measure politics using NLP and AI techniques.
+
+
+Testing
+=======
+
+To run all tests type:
+
+    pytest tests
+
+To run specific test put path to the test file:
+
+    pytest tests/t_preprocessors/test_text_cleaners.py
---
+++ tests/t_preprocessors/test_text_cleaners.py
@@ -0,0 +1,47 @@
+import pytest
+from aipolit.preprocessors.text_cleaners import \
+     remove_emojis, \
+     remove_emails, \
+     remove_urls
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Text without emoji", "Text without emoji"),
+        ("No emoji but traditional emoticon :-)", "No emoji but traditional emoticon :-)"),
+        ("Text and emoji 😀", "Text and emoji "),
+        ("Text and 😀 emoji", "Text and  emoji"),
+        ("Text and😀emoji", "Text andemoji"),
+        ("😀 Text and emoji", " Text and emoji"),
+        ("😀", ""),
+    ])
+def test_remove_emojis(input_text, expected_text):
+    assert expected_text == remove_emojis(input_text)
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Text without url", "Text without url"),
+        ("www.example.com", "www.example.com"), # not supported by default
+        ("http://www.example.com", ""),
+        ("https://www.example.com", ""),
+        ("HTTPS://www.example.com", ""),
+        ("HTTPS://www.example.com/a/a/v.html", ""),
+        ("HTTPS://www.example.com/a/a/v.html?a=4&c=1", ""),
+        ("before HTTPS://www.example.com/a/a/v.html?a=4&c=1 after", "before  after"),
+    ])
+def test_remove_urls(input_text, expected_text):
+    assert expected_text == remove_urls(input_text)
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Text without email", "Text without email"),
+        ("john@example.com", ""),
+        ("before john@example.com after", "before  after"),
+    ])
+def test_remove_emails(input_text, expected_text):
+    assert expected_text == remove_emails(input_text)
